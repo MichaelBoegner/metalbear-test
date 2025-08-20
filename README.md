1c15ddb95b9a125670975385dbdb553b70de4f70
“MichaelBoegner” <cmikeb@gmail.com>
2025-08-20 12:19:02 +0700

    redis replica image in original metalbear walkthrough image was broken and not storing items from master. Pointing reads to master now and will containerize and deploy my own image of the code for guestbook so I can have both cluster and local functionality, since cluster was not showing guest lists due to issue.

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# metalbear-test
-Learning about metalbear
+# Debugging Golang Applications with mirrord
+
+<div align="center">
+  <a href="https://mirrord.dev">
+    <img src="images/mirrord.svg" width="150" alt="mirrord Logo"/>
+  </a>
+  <a href="https://go.dev">
+    <img src="images/go.svg" width="150" alt="Go Logo"/>
+  </a>
+</div>
+
+## Overview
+
+This is a sample Guestbook application built with Go and Redis to demonstrate debugging Kubernetes applications using mirrord. The application allows users to write messages to a guestbook, which are stored in Redis and displayed on the web interface.
+
+## Prerequisites
+
+- Go 1.23.2 or higher
+- Docker and Docker Compose
+- Kubernetes cluster
+- mirrord CLI installed
+
+## Quick Start
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/waveywaves/mirrord-go-debug-example
+cd mirrord-go-debug-example
+```
+
+2. Deploy to Kubernetes:
+
+```bash
+kubectl create -f ./kube
+```
+
+3. Debug with mirrord:
+
+```bash
+mirrord exec -t deployment/guestbook go run main.go
+```
+
+The application will be available at http://localhost:3000
+
+## Architecture
+
+The application consists of:
+- Go web server
+- Redis master for write operations
+- Redis replica for read operations
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ main.go
@@ -0,0 +1,155 @@
+/*
+Copyright 2014 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/xyproto/simpleredis/v2"
+)
+
+var (
+	masterPool *simpleredis.ConnectionPool
+	replicaPool  *simpleredis.ConnectionPool
+)
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func writeError(w http.ResponseWriter, err error, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(ErrorResponse{Error: err.Error()})
+}
+
+func ListRangeHandler(rw http.ResponseWriter, req *http.Request) {
+	key := mux.Vars(req)["key"]
+	list := simpleredis.NewList(masterPool, key)
+	
+	members, err := list.GetAll()
+	if err != nil {
+		writeError(rw, fmt.Errorf("failed to get list: %v", err), http.StatusServiceUnavailable)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(rw).Encode(members); err != nil {
+		writeError(rw, fmt.Errorf("failed to encode response: %v", err), http.StatusInternalServerError)
+		return
+	}
+}
+
+func ListPushHandler(rw http.ResponseWriter, req *http.Request) {
+	key := mux.Vars(req)["key"]
+	value := mux.Vars(req)["value"]
+	list := simpleredis.NewList(masterPool, key)
+	
+	if err := list.Add(value); err != nil {
+		writeError(rw, fmt.Errorf("failed to add to list: %v", err), http.StatusServiceUnavailable)
+		return
+	}
+	
+	ListRangeHandler(rw, req)
+}
+
+func InfoHandler(rw http.ResponseWriter, req *http.Request) {
+	info, err := masterPool.Get(0).Do("INFO")
+	if err != nil {
+		writeError(rw, fmt.Errorf("failed to get Redis info: %v", err), http.StatusServiceUnavailable)
+		return
+	}
+	
+	rw.Header().Set("Content-Type", "text/plain")
+	rw.Write(info.([]byte))
+}
+
+func EnvHandler(rw http.ResponseWriter, req *http.Request) {
+	environment := make(map[string]string)
+	for _, item := range os.Environ() {
+		splits := strings.Split(item, "=")
+		key := splits[0]
+		val := strings.Join(splits[1:], "=")
+		environment[key] = val
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(rw).Encode(environment); err != nil {
+		writeError(rw, fmt.Errorf("failed to encode environment: %v", err), http.StatusInternalServerError)
+		return
+	}
+}
+
+func initializeRedisConnection(host string, maxRetries int) (*simpleredis.ConnectionPool, error) {
+	var pool *simpleredis.ConnectionPool
+	var err error
+	
+	for i := 0; i < maxRetries; i++ {
+		pool = simpleredis.NewConnectionPoolHost(host)
+		// Test the connection
+		_, err = pool.Get(0).Do("PING")
+		if err == nil {
+			return pool, nil
+		}
+		
+		log.Printf("Failed to connect to Redis at %s (attempt %d/%d): %v", host, i+1, maxRetries, err)
+		if i < maxRetries-1 {
+			time.Sleep(2 * time.Second)
+		}
+	}
+	
+	return nil, fmt.Errorf("failed to connect to Redis at %s after %d attempts: %v", host, maxRetries, err)
+}
+
+func main() {
+	const maxRetries = 5
+	
+	var err error
+	masterPool, err = initializeRedisConnection("redis-master:6379", maxRetries)
+	if err != nil {
+		log.Fatalf("Failed to initialize Redis master connection: %v", err)
+	}
+	defer masterPool.Close()
+	
+	replicaPool, err = initializeRedisConnection("redis-replica:6379", maxRetries)
+	if err != nil {
+		log.Fatalf("Failed to initialize Redis replica connection: %v", err)
+	}
+	defer replicaPool.Close()
+
+	r := mux.NewRouter()
+	r.Path("/lrange/{key}").Methods("GET").HandlerFunc(ListRangeHandler)
+	r.Path("/rpush/{key}/{value}").Methods("GET").HandlerFunc(ListPushHandler)
+	r.Path("/info").Methods("GET").HandlerFunc(InfoHandler)
+	r.Path("/env").Methods("GET").HandlerFunc(EnvHandler)
+
+	n := negroni.Classic()
+	n.UseHandler(r)
+	
+	log.Println("Starting server on :3000")
+	if err := http.ListenAndServe(":3000", n); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
